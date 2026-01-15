b61782ac0c89a6ff93a15e326a41517f5da4b262
Aroohi Sharma <164870609+aroohisharma06@users.noreply.github.com>
2026-01-15 21:37:13 +0530

    Update path_utils.py

--- core/path_utils.py
+++ core/path_utils.py
@@ -4,4 +4,4 @@ def to_virtual_path(abs_path, base_dir):
     """
     Convert absolute path → virtual path (relative to base_dir)
     """
-    return os.path.relpath(str(abs_path), str(base_dir))
+    return os.path.relpath(abs_path , base_dir).replace(os.sep, "/")
