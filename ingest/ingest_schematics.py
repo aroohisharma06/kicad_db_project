3530dc9df689e821f205528230544879533fb59c
Aroohi Sharma <164870609+aroohisharma06@users.noreply.github.com>
2026-01-15 20:35:01 +0530

    ingest_schematics

--- ingest/ingest_schematics.py
+++ ingest/ingest_schematics.py
@@ -1,28 +1,31 @@
 import os
 from datetime import datetime
 from core.parser import parse_schematic
-from core.db import get_connection, create_table, insert_or_update
-from config import SCH_FILES_FOLDER, SCH_FILES_TABLE , DATA_DIR  
-from core.path_utils import to_virtual_path          
+from core.db import get_connection , create_table , insert_or_update
+from config import SCH_FILES_FOLDER , SCH_FILES_TABLE, DATA_DIR
+from core.path_utils import to_virtual_path
 
-def ingest_schematics():
+def ingest_schematics() : 
     conn = get_connection()
-    create_table(conn, SCH_FILES_TABLE)
-
+    create_table(conn,SCH_FILES_TABLE)
     summary = []
 
-    for file in os.listdir(SCH_FILES_FOLDER):
-        if file.endswith(".kicad_sch"):
-            file_path = os.path.join(SCH_FILES_FOLDER, file)
-            virtual_path = to_virtual_path(file_path, DATA_DIR)
-            try:
-                data = parse_schematic(file_path)
-                data["ingested_at"] = datetime.now().isoformat()
-                data["file_path"]= virtual_path
-                insert_or_update(conn, SCH_FILES_TABLE, data)
-                summary.append((file, "SUCCESS"))
-            except Exception as e:
-                summary.append((file, f"ERROR: {e}"))
+for file in os.listdir(SCH_FILES_FOLDER):
+    if file.endswith(".kicad_sch"):
+        file_path = os.path.join(SCH_FILES_FOLDER , file )
+        virtual_path = to_virtual_path(file_path, DATA_DIR)
 
-    conn.close()
-    return summary
+try:
+    data = parse_schematic(file_path)
+    data["ingested_at"] = datetime.now().isoformat()
+    data["file_path"] = virtual_path
+    insert_or_update(conn,SCH_FILES_TABLE,data)
+    summary.append((file,"SUCCESS"))
+except Exception as e :
+    summary.append((file,f"ERROR:{e}"))
+
+conn.close()
+return summary
+
+
+
