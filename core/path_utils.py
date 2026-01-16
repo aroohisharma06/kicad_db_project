24cd6356eeea6d23bb261d28a95aa9f9d4958fb8
Aroohi Sharma <164870609+aroohisharma06@users.noreply.github.com>
2026-01-16 10:29:01 +0530

    Delete core/path_utils.py

--- core/path_utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def to_virtual_path(abs_path, base_dir):
-    """
-    Convert absolute path → virtual path (relative to base_dir)
-    """
-    return os.path.relpath(abs_path , base_dir).replace(os.sep, "/")
